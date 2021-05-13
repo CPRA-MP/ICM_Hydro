ee3e2feda7d8bf5a26d68ff25db18286598f260f
Eric <47019383+edwhite12@users.noreply.github.com>
2021-05-12 22:45:38 -0500

    debug

--- 2D_celldSal.f
+++ 2D_celldSal.f
@@ -41,6 +41,11 @@
           if (Qtrib(ktrib,kday) < 0.0) then
               Saltrib = S(j,1)
           endif             
+          write(*,*) 'kt',ktrib
+          write(*,*) 'kd',kday
+          write(*,*) 'j',j
+          write(*,*) 'saltrib', Saltrib
+          write(*,*) 'qsalsum',QSalsum
           QSalsum = QSalsum - Qtrib(ktrib,kday)*Saltrib*Qmult(j,ktrib)
       enddo
 
