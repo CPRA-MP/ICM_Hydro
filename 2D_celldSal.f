2e7f94e7d98b9c4f49d200e84692f6ebf9afc1c8
Eric <47019383+edwhite12@users.noreply.github.com>
2021-05-24 13:16:05 -0500

    remove max_dsal filter

--- 2D_celldSal.f
+++ 2D_celldSal.f
@@ -194,13 +194,13 @@
           !>> this net precip volumetric change does not need to be included in Qsalsum since precip and ET are 
           !>> always assumed to have zero salinity mass in those water volumes
           
-          !>> set default value that will cap the allowable change in salinity for the current timestep
-          maxDSal = 0.1           ! this dS cap will cap salinity changes to 0.1 ppt but is only applied to forested compartments below
-          
-          ! maxDSal = 0.1*SALAV(j)  ! this dS cap will cap a change in salinity to 10% of the background mean salinity of the compartment
-                                  ! (mean salinity is updated every day using a rolling window that will equal the mean one-day salinity at the end of Jan 1, 
-                                  ! will equal the 6 month salinity on June 30, and so on      
-
+!          !>> set default value that will cap the allowable change in salinity for the current timestep
+!          maxDSal = 0.1           ! this dS cap will cap salinity changes to 0.1 ppt but is only applied to forested compartments below
+!          
+!          ! maxDSal = 0.1*SALAV(j)  ! this dS cap will cap a change in salinity to 10% of the background mean salinity of the compartment
+!                                  ! (mean salinity is updated every day using a rolling window that will equal the mean one-day salinity at the end of Jan 1, 
+!                                  ! will equal the 6 month salinity on June 30, and so on      
+!
 !          !>> check to see if the compartment is 10% open water or less (indicates possibly location in swamp/forested region
 !          !>> if it is small water-to-land ratio, then check to see if it is fresh
 !          !>> if both criteria are met - do not allow the salinity to increase a large amount for the current timestep
