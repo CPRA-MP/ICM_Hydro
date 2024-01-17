8e8daf09fd7a40832dca7c89bdca5352b2539294
MNzwang <zwang@moffattnichol.com>
2024-01-16 21:00:59 -0500

    Update 2D_celldTmp.f
    ICM-15: update temperature under dry condition.
    add debug output

--- 2D_celldTmp.f
+++ 2D_celldTmp.f
@@ -6,8 +6,10 @@
 	
         use params
 
-        real :: QRain, CSHEAT,rhoj,ddy1,ddy2,dddy,ake,aktmp,DTempw2,QTMPsum
-        integer :: kdiv, ktrib,k,iab,jnb,j
+!      implicit none
+        real :: QRain,CSHEAT,rhoj,ddy1,ddy2,dddy,ake,cden,aktmp,DTempw2,QTMPsum
+        real :: vol1,vol2,ddym1,ddym2,marsh_vol1,marsh_vol2,Qlink,CTMPface
+        integer :: kdiv,ktrib,k,iab,jnb,j,kday
 
         CSHEAT=4182.					!Specific heat
         rhoj=1000.*(1+S(j,1)/1000.)		!density
@@ -73,7 +75,7 @@
             endif  
             iab=abs(icc(j,k))
 !          call Temperature(mm,iab,jnb,j,k,QTMPsum)	!Temperature change computations
-            call Temperature(iab,jnb,j,k,QTMPsum)	!Temperature change computations
+            if(iab > 0) call Temperature(iab,jnb,j,k,QTMPsum)	!Temperature change computations
         enddo
 
 
@@ -130,42 +132,64 @@
 !      Tempw(j,2) = ((Tempw(j,1)*As(j,1)*dddy - QTMPsum*dt)
 !     &   / (As(j,1)*dddy +(Qsum_in(j)-Qsum_out(j)+QRain)*dt) ) + DTempw2
 
-        if(vol2 > 0) then
+        if(ddy2 > dry_threshold) then
+!        if(vol2 > 0) then
             Tempw(j,2) = (Tempw(j,1)*vol1 - QTMPsum*dt)/vol2 + DTempw2
         else
             Tempw(j,2) = ta(kday)
         endif
 !	Tempw(j,2)=Tempw(j,1)+DTempw1+DTempw2
-	
-     
       
-!		if(Tempw(j,2).lt.TempMR(kday))Tempw(j,2)=TempMR(kday)
+
+! for code debugging
+        if(((Tempw(j,2))<0) .or. ((Tempw(j,2))>40)) then
+          write(1,*) 'j:',j
+          write(1,*) 'As(j,1):',As(j,1)
+          write(1,*) 'depth(t-1) = ',Es(j,1)-Bed(j)
+          write(1,*) 'depth(t) =', Es(j,2)-Bed(j)
+          write(1,*) 'Dz =',Es(j,2)-Es(j,1)
+          write(1,*) 'vol(t-1) =', vol1,marsh_vol1
+          write(1,*) 'vol(t) =', vol2,marsh_vol2
+          write(1,*) 'Tempw(j,1):',Tempw(j,1)
+          write(1,*) 'Tempw(j,2):',Tempw(j,2)
+          write(1,*) 'QTMPsum:',QTMPsum
+          write(1,*) 'QRain:',QRain
+          write(1,*) 'aktmp:',aktmp, 'DTempw2:',DTempw2
+          write(1,*) 'Tempe(j,kday):',Tempe(j,kday)   
+          write(1,*) 'Air Temp:',ta(kday)   
+          do k=1,nlink2cell(j)
+              iab=abs(icc(j,k))
+              if(icc(j,k) /= 0) then
+                  if(icc(j,k) < 0) then
+                      jnb=jus(iab)
+                  elseif(icc(j,k) > 0) then
+                      jnb=jds(iab)
+                  endif  
+              endif
+              Qlink = sicc(j,k)*Q(iab,2)
+              if(abs(Qlink)>0) then
+                  write(1,*)'LinkID=',iab,'Type=',linkt(iab),'Q=',Qlink
+                  if(Q(iab,2) >= 0.0) then
+                      CTMPface=Tempw(jus(iab),1)
+                  else
+                      CTMPface=Tempw(jds(iab),1)
+                  endif
+                  write(1,*)'Tempw(jus)=',Tempw(jus(iab),1),
+     &                      'Tempw(jds)=',Tempw(jds(iab),1), 'CTMPface=',CTMPface
+                  write(1,*)'QTEMPadvec=',sicc(j,k)*(Q(iab,2))*CTMPface
+                  write(1,*)'QTEMPdiffu=',fe*EAOL(iab)*(Tempw(j,1)-Tempw(jnb,1))
+              endif
+          enddo
+          stop
+        endif
 
 ! low high pass filter
+!		if(Tempw(j,2).lt.TempMR(kday))Tempw(j,2)=TempMR(kday)
         if(Tempw(j,2).lt.2.0)Tempw(j,2)=2.0
 !      if(Tempw(j,2).gt.36.)Tempw(j,2)=36.
 !        if(Tempw(j,2).gt.tmpmax)Tempw(j,2)=tmpmax  !temperature instable
         if(Tempw(j,2).gt.ta(kday))Tempw(j,2)=ta(kday)
 
-        if(isNan(Tempw(j,2))) then      ! YW
-          write(*,*) 'j:',j
-          write(*,*) 'Tempw(j,2):',Tempw(j,2)
-          write(*,*) 'Tempw(j,1):',Tempw(j,1)
-          write(*,*) 'As(j,1):',As(j,1)
-          write(*,*) 'dddy:',dddy
-          write(*,*) 'QTMPsum:',QTMPsum
-          write(*,*) 'dt:',dt
-          write(*,*) 'Qsum_in(j):',Qsum_in(j)
-          write(*,*) 'Qsum_out(j):',Qsum_out(j)
-          write(*,*) 'QRain:',QRain
-          write(*,*) 'DTempw2:',DTempw2
-          write(*,*) 'aktmp:',aktmp
-          write(*,*) 'kday:',kday
-          write(*,*) 'Tempe(j,kday):',Tempe(j,kday)   
-          stop
-	  !write(*,*) 'setting water temp to equil temp due to NaN'
-	  !Tempw(j,2) = Tempe(j,kday)
-        endif
 
 !	fsal=(1+S(j,2)/35) !-EDW not used anywhere									!salinity correction on Vs
 !	Temph(j,2)=Tempw(j,2)+0.5							!JKS 10/31/13
