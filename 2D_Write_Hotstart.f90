5f5588a63e9762c9486eb892055e4e799273d1e2
MNzwang <zwang@moffattnichol.com>
2025-01-29 12:41:22 -0500

    Update 2D_Write_Hotstart.f90
    formatting revision - line indent

--- 2D_Write_Hotstart.f90
+++ 2D_Write_Hotstart.f90
@@ -38,52 +38,52 @@
       write(1,*)'Saving values from last timestep to a hotstart file.'
       write(*,*)'Saving values from last timestep to a hotstart file.'
 
-      write(401,11140)'Compartment',	&
-                      'Stage',		    &
-                      'Salinity',		&
-                      'CSS_sand',		&
-                      'CSS_silt',		&
-                      'CSS_clay',		&
-                      'CSS_clayfloc',	&
-                      'WaterTemp',		&
-                      'NO3_NO2',		&
-                      'NH4',		    &
-                      'DIN',		    &
-                      'OrgN',		    &
-                      'TIP',		    &
-                      'TOC',		    &
-                      'DO',		        &
-                      'LiveAlg',	    &
-                      'DeadAlg',	    &
-                      'DON',		    &
-                      'DOP',		    &
-                      'DIP',		    &
-                      'ChlA',		    &
+      write(401,11140)'Compartment',    &
+                      'Stage',          &
+                      'Salinity',       &
+                      'CSS_sand',       &
+                      'CSS_silt',       &
+                      'CSS_clay',       &
+                      'CSS_clayfloc',   &
+                      'WaterTemp',      &
+                      'NO3_NO2',        &
+                      'NH4',            &
+                      'DIN',            &
+                      'OrgN',           &
+                      'TIP',            &
+                      'TOC',            &
+                      'DO',             &
+                      'LiveAlg',        &
+                      'DeadAlg',        &
+                      'DON',            &
+                      'DOP',            &
+                      'DIP',            &
+                      'ChlA',           &
                       'TKN',            &
                       'Marsh_stage'
 
       do j=1,N
-	      write(401,11142) j,		                    &
-                          Es(j,2),		                &
-                          S(j,2),		                &
-                          Css(j,2,1),		            &
-                          Css(j,2,2),		            &
-                          Css(j,2,3),		            &
-                          Css(j,2,4),		            &
-                          Tempw(j,2),		            &
-                          min(Chem(j,1,2),1000.0),		&
-                          min(Chem(j,2,2),1000.0),		&
-                          min(Chem(j,3,2),1000.0),		&
-                          min(Chem(j,4,2),1000.0),		&
-                          min(Chem(j,5,2),1000.0),		&
-                          min(Chem(j,6,2),1000.0),		&
-                          min(Chem(j,7,2),1000.0),		&
-                          min(Chem(j,8,2),1000.0),		&
-                          min(Chem(j,9,2),1000.0),		&
-                          min(Chem(j,10,2),1000.0),		&
-                          min(Chem(j,11,2),1000.0),		&
-                          min(Chem(j,12,2),1000.0),		&
-                          min(Chem(j,13,2),1000.0),		&
+          write(401,11142) j,                           &
+                          Es(j,2),                      &
+                          S(j,2),                       &
+                          Css(j,2,1),                   &
+                          Css(j,2,2),                   &
+                          Css(j,2,3),                   &
+                          Css(j,2,4),                   &
+                          Tempw(j,2),                   &
+                          min(Chem(j,1,2),1000.0),      &
+                          min(Chem(j,2,2),1000.0),      &
+                          min(Chem(j,3,2),1000.0),      &
+                          min(Chem(j,4,2),1000.0),      &
+                          min(Chem(j,5,2),1000.0),      &
+                          min(Chem(j,6,2),1000.0),      &
+                          min(Chem(j,7,2),1000.0),      &
+                          min(Chem(j,8,2),1000.0),      &
+                          min(Chem(j,9,2),1000.0),      &
+                          min(Chem(j,10,2),1000.0),     &
+                          min(Chem(j,11,2),1000.0),     &
+                          min(Chem(j,12,2),1000.0),     &
+                          min(Chem(j,13,2),1000.0),     &
                           min(Chem(j,14,2),1000.0),     & ! chem unit = mg/L
                           Eh(j,2)
       enddo
