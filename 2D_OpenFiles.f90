300d77766a5019369aaf5bb2f7712432462238c7
MNzwang <zwang@moffattnichol.com>
2025-01-29 12:43:59 -0500

    Update 2D_OpenFiles.f90
    formatting revision - line indent

--- 2D_OpenFiles.f90
+++ 2D_OpenFiles.f90
@@ -12,11 +12,11 @@
 
       open (unit=32, file= 'Cells.csv', status = 'unknown')
       open (unit=323, file ='Fetch.csv', status = 'unknown')
-      open (unit=33, file= 'Links.csv', status = 'unknown')			! JAM Oct 2010
+      open (unit=33, file= 'Links.csv', status = 'unknown')         ! JAM Oct 2010
       open (unit=34, file= 'LinksClosedHours.dat', status = 'unknown') !-EDW !value of 1 means link is closed for the hour, zero means it is open
       open (unit=35,file='LockControlObservedData.csv',status='unknown')
 !      open (unit=36, file= 'SWR.dat', status = 'unknown')
-      open (unit=74, file= 'MissRToC.csv', status = 'unknown')		! JAM Oct 2010
+      open (unit=74, file= 'MissRToC.csv', status = 'unknown')      ! JAM Oct 2010
       open (unit=39, file= 'TribQ.csv', status = 'unknown')   !Tributary flow (m3/s)
       open (unit=40, file= 'PET.csv', status = 'unknown')
       open (unit=42, file= 'Precip.csv', status='unknown')
@@ -27,33 +27,33 @@
       open (unit=48, file= 'TideTranspose.csv',status='unknown')
       open (unit=49, file= 'TideWeight.csv',status='unknown')
       open (unit=77, file= 'QMult.csv', form= 'formatted',status = 'unknown')
-      open (unit=55, file= 'TribS.csv', status = 'unknown')	! Tributary sand concentration (mg/L)
+      open (unit=55, file= 'TribS.csv', status = 'unknown') ! Tributary sand concentration (mg/L)
       open (unit=555,file= 'TribF.csv',status='unknown')      ! Tributary fines concentration (mg/L)
       open (unit=56, file= 'SBC.dat', status = 'unknown')
       open (unit=110, file= 'surge.csv', form = 'formatted')
-      open (unit=125, file= 'KBC.dat', status = 'unknown')		        !node numbers of open boundary
+      open (unit=125, file= 'KBC.dat', status = 'unknown')              !node numbers of open boundary
       open (unit=101, file= 'BCToC2.dat', form = 'formatted')
 
 !==== conditional input files
-	  if (Ndiv>0) then
-          open (unit=86, file= 'DivQm.csv', status ='unknown')	! diversion flow multiplier on Miss Riv flow
+      if (Ndiv>0) then
+          open (unit=86, file= 'DivQm.csv', status ='unknown')  ! diversion flow multiplier on Miss Riv flow
           open (unit=88, file= 'QMult_div.csv', form= 'formatted',status ='unknown')
           open (unit=89, file= 'DivSW.csv', status = 'unknown')
-	  endif
-!      open (unit=117, file= 'AsedOW.csv',form ='formatted',status ='unknown')		! Sediment Accretion  !Status='unknown' added by Joao Pereira 5/17/2011
-	  if (nlinksw>0) then
-          open (unit=126, file= 'links_to_write.csv',status='unknown')	    !input csv file with the link ID numbers of links to write flowrate to output file
-	  endif
-	  if (nstghr>0) then
+      endif
+!      open (unit=117, file= 'AsedOW.csv',form ='formatted',status ='unknown')      ! Sediment Accretion  !Status='unknown' added by Joao Pereira 5/17/2011
+      if (nlinksw>0) then
+          open (unit=126, file= 'links_to_write.csv',status='unknown')      !input csv file with the link ID numbers of links to write flowrate to output file
+      endif
+      if (nstghr>0) then
           open (unit=127, file='hourly_stage_to_write.csv',status='unknown')  !input csv file with the ID numbers of compartments to write hourly stage to output file
       endif
       if (nlinklimiter>0) then
-	      open (unit=500, file= 'links_to_apply.csv',status='unknown')
-	  endif
+          open (unit=500, file= 'links_to_apply.csv',status='unknown')
+      endif
 
-!     If WQ modeling is excluded (iWQ=0), WQ inputs are disabled	  
-	  if (iWQ>0) then
-          open (unit=44, file= 'AnthL.csv', status = 'unknown')			! Farm and Urban WW Loads (kg/d)
+!     If WQ modeling is excluded (iWQ=0), WQ inputs are disabled      
+      if (iWQ>0) then
+          open (unit=44, file= 'AnthL.csv', status = 'unknown')         ! Farm and Urban WW Loads (kg/d)
           open (unit=80, file= 'NO2NO3Data.csv', status = 'unknown')
           open (unit=81, file= 'NH4Data.csv', status = 'unknown')
           open (unit=82, file= 'OrgNData.csv', status = 'unknown')
@@ -63,55 +63,55 @@
           open (unit=87, file= 'DivWQ.csv', status ='unknown')
 !          open (unit=118, file= 'UplandNP.dat', form ='formatted')
       endif
-	  
+      
 !     If 1D2D coupling enabled, 1D2Dcoupling input files
       if (n1D>0) then
           if (ntc>0) then
-     	      open (unit=402, file= '1D2Dcoupling_tc.csv', status = 'unknown')
-		  endif
+              open (unit=402, file= '1D2Dcoupling_tc.csv', status = 'unknown')
+          endif
           if (nlc>0) then
               open (unit=403, file= '1D2Dcoupling_lc.csv', status = 'unknown')
-		  endif
+          endif
           if (nuc>0) then
               open (unit=404, file= '1D2Dcoupling_uc.csv', status = 'unknown')
- 		  endif
-      endif	  
-	  
+          endif
+      endif   
+      
 !     If idt_schm = 2 user specified varying time step input file in daily interval
       if(idt_schm == 2) then
           open (unit=900, file= 'Dt_Varying_user.csv', status = 'unknown')
       endif
 
 !>> Open output text files (in append mode, if needed).
-      open (unit=75,file='SAL.out',form ='formatted',position='append')			! Salinity.out
+      open (unit=75,file='SAL.out',form ='formatted',position='append')         ! Salinity.out
       open (unit=96,file='TSS.out',form='formatted',position='append')
-      open (unit=100,file='TMP.out',form='formatted',position='append')		   
-	  open (unit=103,file='SedAcc.out',form='formatted',position='append')		! Last row will be used to compute open water Acc. 
-	  open (unit=104,file='SedAcc_MarshInt.out',form='formatted',position='append')		! Last row will be used to compute interior marsh Acc. 
-	  open (unit=1045,file='SedAcc_MarshEdge.out',form='formatted',position='append')		! Last row will be used to compute marsh edge Acc. 
+      open (unit=100,file='TMP.out',form='formatted',position='append')        
+      open (unit=103,file='SedAcc.out',form='formatted',position='append')      ! Last row will be used to compute open water Acc. 
+      open (unit=104,file='SedAcc_MarshInt.out',form='formatted',position='append')     ! Last row will be used to compute interior marsh Acc. 
+      open (unit=1045,file='SedAcc_MarshEdge.out',form='formatted',position='append')       ! Last row will be used to compute marsh edge Acc. 
       open (unit=105,file='fflood.out',form='formatted',position='append')
-      open (unit=111,file='STG.out',form='formatted',position='append')			! ESAVE.OUT
-      open (unit=112,file='TRG.out',form='formatted',position='append')			! Range.out
+      open (unit=111,file='STG.out',form='formatted',position='append')         ! ESAVE.OUT
+      open (unit=112,file='TRG.out',form='formatted',position='append')         ! Range.out
       open (unit=124,file='FLOm.out',form='formatted',position='append')
-      open(unit=210,file='STGhr.out',form='formatted',position='append')				!output file for hourly water level in Boundary Condition cells
-      open(unit=211,file='FLO.out',form='formatted',position='append')		!output file for flowrate	
+      open(unit=210,file='STGhr.out',form='formatted',position='append')                !output file for hourly water level in Boundary Condition cells
+      open(unit=211,file='FLO.out',form='formatted',position='append')      !output file for flowrate   
       open(unit=212,file='STGm.out',form='formatted',position='append')
       open (unit=93,file='O2Sat.out',form='formatted',position='append')
 
-!     If WQ modeling is excluded (iWQ=0), WQ outputs are disabled	  
-	  if (iWQ>0) then
+!     If WQ modeling is excluded (iWQ=0), WQ outputs are disabled     
+      if (iWQ>0) then
           open (unit=70,file='DIN.out',form ='formatted',position='append')
           open (unit=71,file='OrgN.out',form='formatted',position='append')
-          open (unit=72,file='TPH.out',form='formatted',position='append')			! TP.out
+          open (unit=72,file='TPH.out',form='formatted',position='append')          ! TP.out
           open (unit=73,file='TOC.out',form='formatted',position='append')
-          open (unit=91,file='NO3.out',form='formatted',position='append')			! NO2NO3.out
-          open (unit=92,file='NH4.out',form = 'formatted',position='append')		
+          open (unit=91,file='NO3.out',form='formatted',position='append')          ! NO2NO3.out
+          open (unit=92,file='NH4.out',form = 'formatted',position='append')        
           open (unit=94,file='ALG.out',form='formatted',position='append')
           open (unit=95,file='DO.out',form='formatted',position='append')
-          open (unit=97,file='DET.out',form='formatted',position='append')			! DeadAlgae.out
+          open (unit=97,file='DET.out',form='formatted',position='append')          ! DeadAlgae.out
           open (unit=113,file='DON.out',form='formatted',position='append')
-          open (unit=119,file='SPH.out',form='formatted',position='append')			! SRP.out
-          open (unit=121,file='NRM.out',form='formatted',position='append')			! NRAcc.out -> Denitrification
+          open (unit=119,file='SPH.out',form='formatted',position='append')         ! SRP.out
+          open (unit=121,file='NRM.out',form='formatted',position='append')         ! NRAcc.out -> Denitrification
           open (unit=123,file='TKN.out',form='formatted',position='append')
       endif
 ! read in information for grid cells used to pass data to other ICM routines !-EDW
